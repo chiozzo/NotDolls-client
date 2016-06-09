32bc80a8ab764e300d0eedbcc4b5bbbb8b5cc4b3
Martin Cross <mncross@gmail.com>
2016-06-09 15:47:52 -0500

    build delete button and DELETE method

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -7,5 +7,12 @@ NotDolls.controller('MainController', [
 
     $http
       .get('http://localhost:5000/api/Inventory')
-      .success(inv => $scope.figurines = inv)
+      .success(inv => $scope.figurines = inv);
+
+    $scope.deleteFigurine = function (InventoryId) {
+      $http.delete(`http://localhost:5000/api/Inventory/${InventoryId}`)
+        .then(
+          response => console.log("resolve", response),
+          response => console.log("reject", response)
+        )}
 }]);
